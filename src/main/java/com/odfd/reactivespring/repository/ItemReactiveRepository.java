0ca5a6fbfcbbf4e77fc718231b6c2469bf9e503a
oflorez <v-oflorez@expedia.com>
2020-07-22 21:51:41 -0400

    Exercises from 33 to 41
    40. Build the Reactive Item Mongo Repository
    41. Reactive Mongo Repository operations using JUNIT  Read  Part1

---
+++ src/main/java/com/odfd/reactivespring/repository/ItemReactiveRepository.java
@@ -0,0 +1,13 @@
+package com.odfd.reactivespring.repository;
+
+import com.odfd.reactivespring.document.Item;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
+
+public interface ItemReactiveRepository extends ReactiveMongoRepository<Item, String> {
+
+    Flux<Item> findByDescription(String description);
+
+
+
+}
---
+++ src/test/java/com/odfd/reactivespring/repository/ItemReactiveRepositoryTest.java
@@ -0,0 +1,67 @@
+package com.odfd.reactivespring.repository;
+
+import com.odfd.reactivespring.document.Item;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataMongoTest
+@ExtendWith(SpringExtension.class)
+class ItemReactiveRepositoryTest {
+
+    @Autowired
+    ItemReactiveRepository itemReactiveRepository;
+
+    List<Item> itemList = Arrays.asList(new Item(null, "Samsung TV", 400.0),
+            new Item(null, "LG TV",420.0),
+            new Item(null, "Apple Watch",299.99),
+            new Item(null, "Beats Headphones",149.99),
+            new Item("ABC", "Bose Headphones",149.99)
+    );
+
+    @BeforeEach
+    public void setUp() {
+        itemReactiveRepository.deleteAll()
+                .thenMany(Flux.fromIterable(itemList))
+                .flatMap(itemReactiveRepository::save)
+                .doOnNext((item -> {
+                    System.out.println("Inserted Item is : " + item);
+                }))
+                .blockLast();
+    }
+
+    @Test
+    public void getAllItems(){
+        StepVerifier.create(itemReactiveRepository.findAll())
+                .expectSubscription()
+                .expectNextCount(5)
+                .verifyComplete();
+    }
+
+    @Test
+    public void getItemById(){
+        StepVerifier.create(itemReactiveRepository.findById("ABC"))
+                .expectSubscription()
+                .expectNextMatches((item -> item.getDescription().equals("Bose Headphones")))
+                .verifyComplete();
+    }
+
+    @Test
+    public void findItemByDescription(){
+        StepVerifier.create(itemReactiveRepository.findByDescription("Bose Headphones").log("findItemByDescription :> "))
+                .expectSubscription()
+                .expectNextCount(1)
+                .verifyComplete();
+    }
+
+}
