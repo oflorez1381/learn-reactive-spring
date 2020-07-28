7336de36dd5790effc0a3161583273194073ecb6
oflorez <v-oflorez@expedia.com>
2020-07-27 21:57:22 -0400

    45. Build Artifact

--- src/test/java/com/odfd/reactivespring/LearnReactiveSpringApplicationTests.java
+++ src/test/java/com/odfd/reactivespring/LearnReactiveSpringApplicationTests.java
@@ -1,9 +1,10 @@
 package com.odfd.reactivespring;
 
+import com.odfd.reactivespring.handler.SamplerHandlerFunction;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SamplerHandlerFunction.class)
 class LearnReactiveSpringApplicationTests {
 
 	@Test
