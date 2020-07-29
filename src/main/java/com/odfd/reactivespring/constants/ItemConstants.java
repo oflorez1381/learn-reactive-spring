02500213503a7d16c0ffb4079aa4f7caf6274047
oflorez <v-oflorez@expedia.com>
2020-07-28 20:06:57 -0400

    47. Initialize the data using CommandLineRunner

---
+++ src/main/java/com/odfd/reactivespring/constants/ItemConstants.java
@@ -0,0 +1,6 @@
+package com.odfd.reactivespring.constants;
+
+public class ItemConstants {
+
+    public static final String ITEM_END_POINT_V1 = "/v1/items";
+}
---
+++ src/main/java/com/odfd/reactivespring/controller/v1/ItemController.java
@@ -0,0 +1,29 @@
+package com.odfd.reactivespring.controller.v1;
+
+import com.odfd.reactivespring.constants.ItemConstants;
+import com.odfd.reactivespring.document.Item;
+import com.odfd.reactivespring.repository.ItemReactiveRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import static com.odfd.reactivespring.constants.ItemConstants.ITEM_END_POINT_V1;
+
+@RestController
+@Slf4j
+public class ItemController {
+
+    ItemReactiveRepository itemReactiveRepository;
+
+    public ItemController(ItemReactiveRepository itemReactiveRepository) {
+        this.itemReactiveRepository = itemReactiveRepository;
+    }
+
+    @GetMapping(ITEM_END_POINT_V1)
+    public Flux<Item> getAllItems(){
+        return itemReactiveRepository.findAll();
+    }
+
+}
