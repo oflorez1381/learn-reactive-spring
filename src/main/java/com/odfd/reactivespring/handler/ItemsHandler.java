62234b77741d662b491f9f9144ede6af47f18ba8
oflorez <v-oflorez@expedia.com>
2020-07-30 21:10:01 -0400

    56. Get All Items Functional Endpoint

--- src/main/java/com/odfd/reactivespring/handler/ItemsHandler.java
+++ src/main/java/com/odfd/reactivespring/handler/ItemsHandler.java
@@ -1,12 +1,23 @@
 package com.odfd.reactivespring.handler;
 
+import com.odfd.reactivespring.document.Item;
 import com.odfd.reactivespring.repository.ItemReactiveRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
 
 @Component
 public class ItemsHandler {
 
     @Autowired
     ItemReactiveRepository itemReactiveRepository;
+
+    public Mono<ServerResponse> getAllItems(ServerRequest serverRequest) {
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(itemReactiveRepository.findAll(), Item.class);
+    }
 }
