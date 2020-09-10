e5cc0d79f339d572739b7337cc78a68713f358a2
oflorez <v-oflorez@expedia.com>
2020-09-10 10:31:18 -0400

    73. Handling Exceptions in Functional Web JUNIT

--- src/test/java/com/odfd/reactivespring/handler/ItemsHandlerTest.java
+++ src/test/java/com/odfd/reactivespring/handler/ItemsHandlerTest.java
@@ -130,4 +130,13 @@ class ItemsHandlerTest {
                 .expectStatus().isNotFound();
     }
 
+    @Test
+    public void runTimeException(){
+        webTestClient.get().uri("/fun/runtimeException")
+                .exchange()
+                .expectStatus().is5xxServerError()
+                .expectBody()
+                .jsonPath("$.message","RuntimeException occurred");
+    }
+
 }
